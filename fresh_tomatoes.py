47ecee4be31ba3565ed4578609288eb4a23a2297
BhagyaLakshmi518 <15pa1a0518@vishnu.edu.in>
2018-05-22 20:58:12 +0530

    Update fresh_tomatoes.py

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -1,7 +1,8 @@
-print("Content-type:text/html \n")
 import re
 import os
 import webbrowser
+print("Content-type:text/html \n")
+
 
 main_page_head = '''
 <DOCTYPE html>
@@ -17,7 +18,7 @@ main_page_head = '''
   margin-top:20px;
   padding:20px
 }
-img{ 
+img{
       width:100%;
       height:100%;
      /* overflow: hidden;*/
@@ -38,11 +39,11 @@ img{
   background-color:lightseagreen;
 }
 .overlay {
-  position: relative; 
-  bottom: 30px; 
+  position: relative;
+  bottom: 30px;
   background: rgb(0, 0, 0);
   background: rgba(0, 0, 0, 0.5); /* Black see-through */
-  color: #f1f1f1; 
+  color: #f1f1f1;
   transition: .5s ease;
   opacity:0;
   color: white;
@@ -50,7 +51,6 @@ img{
   padding: 10px;
   text-align: center;
 }
-
 .a:hover .overlay {
   opacity: 1;
 }
@@ -75,27 +75,25 @@ h1
    color:red;
 }
 </style>
-<script src="https://code.jquery.com/jquery-1.12.3.min.js" integrity="sha256-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ="
+<script src="https://code.jquery.com/jquery-1.12.3.min.js" integrity="sha256"
+            "-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ="
         crossorigin="anonymous"></script>
-    
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/"
+                   "bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/"
+       "bootstrap.min.js"></script>
     <script>
         $(document).ready(function () {
             $("#myModal").on("hidden.bs.modal", function () {
                 $("#iframeYoutube").attr("src", "#");
             })
         })
-
         function changeVideo(vId) {
             var iframe = document.getElementById("iframeYoutube");
             iframe.src = "https://www.youtube.com/embed/" + vId;
-
             $("#myModal").modal("show");
         }
     </script>
-
 </head>'''
 
 
@@ -103,35 +101,41 @@ main_page_content = '''
 <body style="background-color:brown;">
 <h1 style="color:blue;">MOVIE TRAILERS</h1>
 <div class="a" onclick=" changeVideo('20bpjtCbCz0')">
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd39RpQm8W3K_yC8bnb8wJqGzYbkP03mF8z_CK2NpECCjapOAV"/>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd39RpQ"
+          "m8W3K_yC8bnb8wJqGzYbkP03mF8z_CK2NpECCjapOAV"/>
 <div class="overlay">Dead Pool2</div>
 </div>
 <div class="b" onclick=" changeVideo('Wm_vSSlVsV4')">
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUwRKWuYYhNHCFxhdB2MqmhZHQ9ZQL-SrDrtWSlp51cHbDFgG51A"/>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUwRKWuYYh"
+  "NHCFxhdB2MqmhZHQ9ZQL-SrDrtWSlp51cHbDFgG51A"/>
 <div class="overlay">Kaala</div>
 </div>
 <div class="c" onclick=" changeVideo('xjDjIWPwcPU')">
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNb8BPjnh1_8Pxta0HHedtiuT8rR8a6IDbk9elEpzsgvTbtWnu7w"/>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNb8BPjnh1_"
+         "8Pxta0HHedtiuT8rR8a6IDbk9elEpzsgvTbtWnu7w"/>
 <div class="overlay">BlackPanther</div>
 </div>
 <div class="d" onclick=" changeVideo('Gdzif0Px_qY')">
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdGcVcHwBiYd5HGzUbYsKE3QYg8v4F3VJ3_4odLUc9TEBoDBVi"/>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdGcVcHwBiYd"
+         "5HGzUbYsKE3QYg8v4F3VJ3_4odLUc9TEBoDBVi"/>
 <div class="overlay">Chalo</div>
 </div>
 <div class="e" onclick=" changeVideo('k67ErU7SeIE')">
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaI0WvPhZh7n6zhcFH8VzJ_TQFtDln9fKhkEpciXD7sdv16TtfdQ"/>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaI0WvPhZ"
+            "h7n6zhcFH8VzJ_TQFtDln9fKhkEpciXD7sdv16TtfdQ"/>
     <div class="overlay">Band Baja Baarat</div>
 </div>
-<div class="modal fade" id="myModal" tabindex="-1"   aria-labelledby="myModalLabel">
+<div class="modal fade" id="myModal" tabindex="-1"
+           aria-labelledby="myModalLabel">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-body">
-
-                    <iframe id="iframeYoutube" width="555" height="350" src="" frameborder="0" ></iframe>
-
+                    <iframe id="iframeYoutube" width="555" height="350"
+                             src="" frameborder="0" ></iframe>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-default"
+                         data-dismiss="modal">Close</button>
                 </div>
             </div>
         </div>
@@ -149,29 +153,30 @@ id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
 '''
 
 
-def  create_movie(movies):
+def create_movie(movies):
     content = ''
     for movie in movies:
         youtube_id_match = re.search(
-            r'(?<=v=)[^&#]+',movie.trailer_youtube_url)
+            r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
         youtube_id_match = youtube_id_match or re.search(
-            r'(?<=be/)[^&#]+',movie.trailer_youtube_url)
+            r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
         trailer_youtube_url = (youtube_id_match.group(0) if youtube_id_match
-                              else None)
-        content+=movie_title_content.format(
+                               else None)
+        content += movie_title_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
             trailer_youtube_id=trailer_youtube_url
             )
         return content
+
+
 def open_movies_page(movies):
-    output_file=open('fresh_tomatoes.html','w')
-    rendered_content=main_page_content.format(
+    output_file = open('fresh_tomatoes.html', 'w')
+    rendered_content = main_page_content.format(
         movie_tiles=create_movie(movies)
         )
     output_file.write(main_page_head+rendered_content)
     output_file.close()
-    url=os.path.abspath(output_file.name)
-    webbrowser.open('file://'+url,new=2)
-
+    url = os.path.abspath(output_file.name)
+    webbrowser.open('file://'+url, new=2)
         
